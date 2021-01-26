a0b2d2f08b3d3d88c7d37f11af122d86bf008092
czook <czook@iastate.edu>
2021-01-26 17:38:09 +0000

    no errors

--- Assignment0/sandpile.c
+++ Assignment0/sandpile.c
@@ -1,23 +1,23 @@
 // Based off -- https://en.wikipedia.org/wiki/Abelian_sandpile_model
+const int prim =23;
 #include <stdio.h>
  void main(){
-    int grid[23][23]; 
+    int grid[prim][prim]; 
+    //printf("Debug");
  }
 //use this method after worldGen
- void inputVals(int[][] grid,int x, int y, int h){
+ void inputVals(int grid[prim][prim],int x, int y, int h){
     if (x>22 || y>22|| x<0 || y<0){
-        if (x==11 && y=11){
+        if (x==11 && y==11){
             printf("A sink at the center is invalid");
-            retrun -1;
         }
         grid[x][y] = h;
-        
     }
     
  }
 
 
- void worldGen(int[][] grid){
+ void worldGen(int grid[prim][prim]){
     for(int i =0; i<23; i++){
         for(int j =0; j<23; j++){
                 grid[i][j] = 0;
