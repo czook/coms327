761c7bbfbfeb0afb1cf7d7d86c33fa6ba11c7675
czook <czook@iastate.edu>
2021-01-26 18:11:40 +0000

    more work

--- Assignment0/sandpile.c
+++ Assignment0/sandpile.c
@@ -1,9 +1,16 @@
 // Based off -- https://en.wikipedia.org/wiki/Abelian_sandpile_model
 const int prim =23;
+void worldGen(int grid[prim][prim]);
+void printWorld(int grid[prim][prim]);
+void runWorld(int grid[prim][prim]);
+void pilefall(int x, int y, int grid[prim][prim]);
 #include <stdio.h>
- void main(){
+ int main(int argc, char *argv[]){
     int grid[prim][prim]; 
     //printf("Debug");
+    worldGen(grid);
+    printWorld(grid);
+    return 0;
  }
 //use this method after worldGen
  void inputVals(int grid[prim][prim],int x, int y, int h){
@@ -24,3 +31,23 @@ const int prim =23;
         }
     }
  }
+//in the future will use ncurses
+ void printWorld(int grid[prim][prim]){
+     for(int i =0; i<23; i++){
+        for(int j =0; j<23; j++){
+            printf("%d ", grid[i][j]);
+        }
+        printf("\n");
+    }
+ }
+
+ void runWorld(int grid[prim][prim]){
+    
+ }
+//recursive
+ void pilefall(int x, int y, int grid[prim][prim]){
+     if(grid[x][y]<8){
+         //base case
+     }
+ }
+ 
