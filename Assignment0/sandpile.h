b631f3e4f5687e8517168aab36f5ef68b4bef806
Nitman846 <Nitman846@gmail.com>
2021-01-28 19:00:50 -0600

    Create sandpile.h

---
+++ Assignment0/sandpile.h
@@ -0,0 +1,4 @@
+void inputVals(int grid[prim][prim],int x, int y, int h);
+void worldGen(int grid[prim][prim]);
+void printWorld(int grid[prim][prim]);
+int pilefall(int x, int y, int grid[prim][prim]);
