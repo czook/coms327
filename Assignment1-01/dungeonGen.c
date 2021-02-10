f47d83dd53088bbf144e334167569e8ff514e23b
syedal <syedal@pyrite-n2.cs.iastate.edu>
2021-02-10 00:30:08 -0600

    added comments

--- Assignment1-01/dungeonGen.c
+++ Assignment1-01/dungeonGen.c
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 #include <time.h> 
 
-struct Room {
+//room object
+struct Room { 
     int width;
     int height;
-    int x;
-    int y;
-    int xEnd;
-    int yEnd;
+    int x; //x coordinate of top left corner of room
+    int y; //y coordinate of top left corner of room
+    int xEnd; // x coordinate of bottom right corner of room
+    int yEnd; // y coordinate of bottom right corner of room
 };
 
 void genBorder();
 void printBoard();
 void roomGen();
 
+//array to hold all room objects so they can be accessed
 struct Room rooms[6];
+//2D array of our game screen
 char board[21][80];
 
 
@@ -26,7 +29,7 @@ int main(int argc, char* argv[]) {
     
    
 }
-
+//generates border and initializes board with spaces
 void genBorder() {
     for (int i = 0; i < 21; i++) {
         for (int j = 0; j < 80; j++) {
@@ -45,7 +48,7 @@ void genBorder() {
         }
     }
 }
-
+//prints all charcters on board array
 void printBoard() {
     for (int i = 0; i < 21; i++) {
         for (int j = 0; j < 80; j++) {
@@ -54,18 +57,18 @@ void printBoard() {
         printf("\n");
     }
 }
-
+//generates rooms
 void roomGen() {
     srand(time(NULL));
-    for (int i = 0; i <= 6; i++) {
-        rooms[i].width = rand() % (6 - 4 + 1) + 4; ;
+    for (int i = 0; i <= 6; i++) { //iterates through rooms in rooms array and initializes attributes
+        rooms[i].width = rand() % (6 - 4 + 1) + 4; ; //formula is rand() % (upperBound - lowerBound + 1) + lowerBound
         rooms[i].height = rand() % (5 - 3 + 1) + 3; ;
         rooms[i].x = rand() % (70 - 1 + 1) + 1; ;
         rooms[i].y = rand() % (11 - 1 + 1) + 1; ;
         rooms[i].xEnd = rooms[i].x + rooms[i].width;
         rooms[i].yEnd = rooms[i].y + rooms[i].height;
         
-        for (int j = rooms[i].y; j < rooms[i].yEnd; j++) {
+        for (int j = rooms[i].y; j < rooms[i].yEnd; j++) { //places periods on board array for room locations
             for (int k = rooms[i].x; k < rooms[i].xEnd; k++) {
                 
                 board[j][k] = '.';
